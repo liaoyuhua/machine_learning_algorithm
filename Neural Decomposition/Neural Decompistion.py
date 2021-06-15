e51140c7162cffd310375d615f0f293d3a35c136
yh_liao <yh_liao@trip.com>
2021-06-15 15:37:46 +0800

    update Neural Decompistion.py

--- Neural Decomposition/Neural Decompistion.py
+++ Neural Decomposition/Neural Decompistion.py
@@ -1,16 +1,27 @@
 from torch import nn
 import torch
+import numpy as np
 
 class ND(nn.Module):
     def __init__(self, n, units=10, noise=0.001):
         super(ND, self).__init__()
+        # define layers of neural network
         self.wave = nn.Linear(1, n)
         self.unit_linear = nn.Linear(1, units)
         self.unit_softplus = nn.Linear(1, units)
         self.unit_sigmoid = nn.Linear(1, units)
         self.fc = nn.Linear(n + 3 * units, 1)
-        params = dict(self.named_parameters())
-        params['wave']
+        # initialize weights
+        params['wave.weight'].data = torch.from_numpy((2 * np.pi * np.floor(np.arange(n) / 2))[:, np.newaxis]).float()
+        params['wave.bias'].data = torch.from_numpy(np.pi / 2 + np.arange(n) % 2 * np.pi / 2).float()
+        params['unit_linear.weight'].data = torch.from_numpy(np.ones(shape=(units, 1)) + np.random.normal(size=(units, 1)) * noise).float()
+        params['unit_linear.bias'].data = torch.from_numpy(np.random.normal(size=(units)) * noise).float()
+        params['unit_softplus.weight'].data = torch.from_numpy(np.random.normal(size=(units, 1)) * noise).float()
+        params['unit_softplus.bias'].data = torch.from_numpy(np.random.normal(size=(units)) * noise).float()
+        params['unit_sigmoid.weight'].data = torch.from_numpy(np.random.normal(size=(units, 1)) * noise).float()
+        params['unit_sigmoid.bias'].data = torch.from_numpy(np.random.normal(size=(units)) * noise).float()
+        params['fc.weight'].data = torch.from_numpy(np.random.normal(size=(1, n + 3 * units)) * noise).float()
+        params['fc.bias'].data = torch.from_numpy(np.random.normal(size=(1)) * noise).float()
 
     def forward(self, X):
         sin = torch.sin(self.wave(X))
@@ -22,7 +33,7 @@ class ND(nn.Module):
         return out
 
 # define our model
-def model(X, Y, n, units, noise, loss, epochs, batch_size):
+def model(X, Y, n, units, noise, loss, epochs, batch_size, l1_ratio=0.01):
     """
     :param X: 训练features
     :param Y: 训练labels
@@ -42,6 +53,8 @@ def model(X, Y, n, units, noise, loss, epochs, batch_size):
         # forward
         outputs = nd.forward(X)
         loss = loss_fn(outputs, Y)
+        # add L1 regularization
+        loss += l1_ratio * torch.sum(torch.abs(dict(nd.named_parameters())['fc.weight']))
         # backward
         loss.backward()
         # update parameters
@@ -59,19 +72,22 @@ loss_fn = nn.MSELoss()
 optimizer = torch.optim.Adam()
 
 # hyperparameters
-params = {'units': None
-        , 'noise': None
-        , 'epochs': None
-        , 'batch_size': None
+params = {'units': None,
+          'noise': None,
+          'epochs': None,
+          'batch_size': None,
+          'l1_ratio': None
 }
 
 # train model
 n = None
 X = None
 Y = None
+
 nn_model = model(X
                  , Y
                  , params['units']
                  , loss_fn
                  , params['epochs']
-                 , params['batch_size'])
+                 , params['batch_size']
+                 , params['l1_ratio'])
